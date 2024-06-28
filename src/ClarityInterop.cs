76095d44e45d6fda19f730d8c4d735faa99c1725
Jake Soenneker <jake@soenneker.com>
2024-06-28 15:47:11 -0500

    Better initialization

--- src/ClarityInterop.cs
+++ src/ClarityInterop.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
 using Soenneker.Blazor.Clarity.Abstract;
 using Soenneker.Blazor.Utils.ModuleImport.Abstract;
+using Soenneker.Extensions.ValueTask;
+using Soenneker.Utils.AsyncSingleton;
 
 namespace Soenneker.Blazor.Clarity;
 
@@ -14,37 +16,35 @@ public class ClarityInterop : IClarityInterop
     private readonly ILogger<ClarityInterop> _logger;
     private readonly IModuleImportUtil _moduleImportUtil;
 
-    private bool _initialized;
+    private readonly AsyncSingleton<object> _scriptInitializer;
 
     public ClarityInterop(IJSRuntime jSRuntime, ILogger<ClarityInterop> logger, IModuleImportUtil moduleImportUtil)
     {
         _jsRuntime = jSRuntime;
         _logger = logger;
         _moduleImportUtil = moduleImportUtil;
-    }
 
-    private async ValueTask EnsureInitialization(CancellationToken cancellationToken = default)
-    {
-        if (_initialized)
-            return;
+        _scriptInitializer = new AsyncSingleton<object>(async objects =>
+        {
+            var cancellationToken = (CancellationToken)objects[0];
 
-        _initialized = true;
-
-        await _moduleImportUtil.Import("Soenneker.Blazor.Clarity/js/clarityinterop.js", cancellationToken);
-        await _moduleImportUtil.WaitUntilLoaded("Soenneker.Blazor.Clarity/js/clarityinterop.js", cancellationToken);
+            await _moduleImportUtil.Import("Soenneker.Blazor.Clarity/clarityinterop.js", cancellationToken);
+            await _moduleImportUtil.WaitUntilLoadedAndAvailable("Soenneker.Blazor.Clarity/clarityinterop.js", "ClarityInitializer", 100, cancellationToken);
+            return new object();
+        });
     }
 
     public async ValueTask Init(string key, CancellationToken cancellationToken = default)
     {
         _logger.LogDebug("Initializing Clarity...");
 
-        await EnsureInitialization(cancellationToken);
+        await _scriptInitializer.Get(cancellationToken).NoSync();
 
         await _jsRuntime.InvokeVoidAsync("ClarityInitializer.init", cancellationToken, key);
     }
 
     public ValueTask DisposeAsync()
     {
-        return _moduleImportUtil.DisposeModule("Soenneker.Blazor.Clarity/js/clarityinterop.js");
+        return _moduleImportUtil.DisposeModule("Soenneker.Blazor.Clarity/clarityinterop.js");
     }
 }
